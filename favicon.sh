d944eb2bfde01ec6a65c504e803153cc71f2ca77
Bart Massey <bart@cs.pdx.edu>
2007-12-03 15:13:42 -0800

    Netpbm-based favicon builder

---
+++ favicon.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+SCALE='pnmscale -xysize $SIZE $SIZE $BASE.ppm'
+SCALE_MASK='pnmscale -xysize $SIZE $SIZE $BASE-mask.pgm'
+case $1 in
+--prescaled)
+  SCALE='pngtopnm -background \#ffffff $BASE-$SIZE.png'
+  SCALE_MASK='pngtopnm -alpha $BASE-$SIZE.png'
+  shift
+  ;;
+esac  
+SRC=$1
+BASE=`echo $SRC | sed 's/\.[^.]*$//'`
+pngtopnm -background '#ffffff' $SRC > $BASE.ppm
+pngtopnm -alpha $SRC > $BASE-mask.pgm
+LAYERS=""
+while read SIZE COLORS
+do
+  eval $SCALE |
+   ppmquant $COLORS > $BASE-$SIZE.ppm 2>/dev/null
+  eval $SCALE_MASK > $BASE-$SIZE-mask.pgm
+  LAYERS="$LAYERS $BASE-$SIZE.ppm $BASE-$SIZE-mask.pgm"
+done <<'EOF'
+16 16
+32 16
+48 256
+EOF
+ppmtowinicon -andpgms $LAYERS > $BASE-favicon.ico
